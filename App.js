841106d1151bba8c542231f75e07770c625f8226
hotheadhacker <isalmanqureshi@gmail.com>
2020-03-17 16:16:52 +0530

    but the problem is with toolbar stickiness

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { Card, Subheader, Button, ActionButton, Divider, Icon, BottomNavigation, Toolbar  } from 'react-native-material-ui';
+
+
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Welcome to the webCopier APP2!</Text>
+      <Toolbar style={styles.newStyl}
+        leftElement="menu"
+        centerElement="WebCopier - Alpha"
+        searchable={{
+          autoFocus: true,
+          placeholder: 'Search',
+        }}
+        rightElement={{
+            menu: {
+                icon: "more-vert",
+                labels: ["item 1", "item 2"]
+            }
+        }}
+        onRightElementPress={ (label) => { console.log(label) }}
+      />
+      <Text>Welcome to the webCopier APP3!</Text>
+      <Divider/>
+      <Text></Text>
+      
+      <Divider/>
+      <Icon name="web"/>
+      <Card>
+        <Text>Enter URL of a website to clone</Text>
+      </Card>
+      <Button raised primary text="Copy &amp; Save" />
+      <Subheader text="Files will be stored to .local/downloads" />
+
+
+      <ActionButton /> 
+
+      
     </View>
+
+    
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4F6D7A',
+    backgroundColor: 'silver',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
+
+
