6353dfa473f12a971a4ced28d97baaaa1f10a00a
hotheadhacker <isalmanqureshi@gmail.com>
2020-03-17 13:49:39 +0530

    added vector icons mdesign

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the webCopier APP!</Text>
+      <Text>Welcome to the webCopier APP2!</Text>
     </View>
   );
 }
